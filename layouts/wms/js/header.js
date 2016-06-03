34b796c082b04c0c4a4516914711c347b96cc6f9
George Gomez <georgemgomez1309@gmail.com>
2016-06-03 07:36:10 -0700

    update layout and js
    create parallax effect for header

--- layouts/wms/js/header.js
+++ layouts/wms/js/header.js
@@ -1,9 +1,39 @@
+var header = {
+    headerBkg: $("#header-banner"),
+    headerPosition: -350
+};
+
+var __window = {
+    width: $(window).width(),
+    height: $(window).height()
+};
+
+if(__window.width <= 960 && __window.width >= 600) {
+    header.headerPosition = -250;
+}else if(__window.width <= 599) {
+    header.headerPosition = -150;
+}
+
+$(window).resize(function() {
+    __window.width = $(this).width();
+    if(__window.width <= 960 && __window.width >= 600) {
+        header.headerPosition = -250;
+    }else if(__window.width <= 599) {
+        header.headerPosition = -150;
+    }
+    header.headerBkg.css({'background-position-y':header.headerPosition});
+});
+
+
 $(window).scroll(function() {
-    var winHeight = $(window).scrollTop(),
+    var height = $(this).scrollTop(),
+        title = $("#wms-title"),
         menuBar = $("nav.main-header-nav"),
-        headerHeight = $("#header-banner").height();
+        headerHeight = header.headerBkg.height();
         bodyContent = $("#body-content");
-    if(winHeight > headerHeight){
+    header.headerBkg.css({'background-position-y': header.headerPosition + (0.2*height) + 'px'});
+    title.css({'margin-top':(0.8*height) + 'px'});
+    if(height > headerHeight){
         menuBar.addClass('show-nav');
         bodyContent.css({'margin-top':menuBar.height()});
     }else{
@@ -11,7 +41,11 @@ $(window).scroll(function() {
         bodyContent.css({'margin-top':'initial'});
     }
 });
+
+
+
 $(document).ready(function(){
-	$(".side-nav-bar-dropdown-button").dropdown({hover:true,belowOrigin:true});
-	$(".dropdown-button").dropdown({hover:false,belowOrigin:true});
+    header.headerBkg.css({'background-position-y':header.headerPosition});
+    $(".side-nav-bar-dropdown-button").dropdown({hover:true,belowOrigin:true});
+    $(".dropdown-button").dropdown({hover:false,belowOrigin:true});
 });
